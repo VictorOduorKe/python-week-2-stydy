0c67bbf6d10d1a3e453b6861380476e3852f9b69
VictorOduorke <victoroduorxh2@gmail.com>
2025-08-06 10:55:52 +0300

    added stack.py

---
+++ stack.py
@@ -0,0 +1,42 @@
+#-----LIFO----(last in first out)--------
+
+class Stack:
+    def __init__(self):
+        self.items=[]
+
+    def is_empty(self):
+        return len(self.items)==0
+    
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            return None
+
+    def peek(self):  
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            return None
+    def size(self):
+        return len(self.items)
+
+my_stack=Stack()
+my_stack.push(10)          
+my_stack.push(20)          
+my_stack.push(30)          
+my_stack.push(40)   
+
+print(my_stack.pop())
+print(my_stack.pop())
+print(my_stack.peek())
+print(my_stack.is_empty())
+print(my_stack.pop())
+print(my_stack.is_empty())
+print(my_stack.pop())
+print(my_stack.is_empty())
+print(my_stack.pop())
+
